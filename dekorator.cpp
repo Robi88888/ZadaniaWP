3e474116bd3987f4c72f2dcf6b905abd9d48d394
Robi88888 <34077835+Robi88888@users.noreply.github.com>
2018-03-12 18:18:38 +0100

    Standartowa gra
    Stworzenie standartowej gry

---
+++ dekorator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>;
+#include <string>;
+using namespace std;
+
+
+// Standardowa gra
+class Gra {
+	protected:
+		string gra;
+		double koszt;
+
+public:
+	Gra() {
+		gra = "Wersja podstawowa";
+		koszt = 0;
+	}
+
+};
+
+int main() {
+
+}
