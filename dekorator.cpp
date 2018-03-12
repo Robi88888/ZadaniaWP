2221826866271e90d37561dd9e4781bc26b82da7
Robi88888 <34077835+Robi88888@users.noreply.github.com>
2018-03-12 18:47:05 +0100

    Dodanie dodatków
    Dodanie dodatkowej mapy
    Dodanie dodatkowej broni

--- dekorator.cpp
+++ dekorator.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 // Standardowa gra
 class Gra {
-	protected:
+
+protected:
 		string gra;
 		double koszt;
 
@@ -18,25 +19,29 @@ public:
 	virtual string about() {
 		return gra;
 	}
+
+	virtual double cena() {
+		return koszt;
+	}
 };
 
 //dekorator
 class Dekorator : public Gra {
 
-	public:
-		virtual string about() {
-			return "Main Level";
-		}
+public:
+	virtual string about() {
+		return "Main Level";
+	}
 };
 
 //przyk³adowe gry
 class RPG : public Gra {
 
-	public:
-		RPG() :Gra() {
-			Gra = "RPG";
-			koszt = 149;
-		}
+public:
+	RPG() :Gra() {
+		Gra = "RPG";
+		koszt = 149;
+	}
 };
 
 class FPS : public Gra {
@@ -46,6 +51,39 @@ public:
 		Gra = "FPS";
 		koszt = 189;
 	}
+};
+
+//DLC do gry
+class ExtraLevel : public Dekorator {
+	Gra *game;
+
+public: ExtraLevel(Gra *gra) :Dekorator() {
+	game = gra;
+}
+
+		string about() {
+			return game->about() + "Dodatkowa mapa!";
+	}
+
+		double cena() {
+			return game->cena() + 20;
+		}
+};
+
+class ExtraWeapon : public Dekorator {
+	Gra *game;
+
+public: ExtraWeapon(Gra *gra) :Dekorator() {
+	game = gra;
+}
+
+		string about() {
+			return game->about() + "Dodatkowa broñ!";
+		}
+
+		double cena() {
+			return game->cena() + 10;
+		}
 };
 
 int main() {
