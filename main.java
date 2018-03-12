84be7c91c843417afecfd23de8fd2e4c11bc6194
Robi88888 <34077835+Robi88888@users.noreply.github.com>
2018-03-12 22:58:22 +0100

    Main

--- main.java
+++ main.java
@@ -1,5 +1,15 @@
 public class main {
 
-
+	public static void main(String[] args) {
+		
+		singleton.getInstance().addToList("1");
+		singleton.getInstance().addToList("2");
+		singleton.getInstance().addToList("3");
+		singleton.getInstance().addToList("4");
+		System.out.println(singleton.getInstance().getList());
+		singleton.getInstance().removeFromList(0);
+		System.out.println(singleton.getInstance().getList());
+		
+	}
 
 }
