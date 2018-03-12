cc7dab159aa4eddc844f4b197db96d4d07d9da85
Robi88888 <34077835+Robi88888@users.noreply.github.com>
2018-03-12 22:54:04 +0100

    Singleton

--- singleton.java
+++ singleton.java
@@ -1,2 +1,28 @@
 import java.util.ArrayList;
 
+public final class singleton {
+
+	private final static singleton singletonInstance = new singleton();
+	private ArrayList<String> list = null;
+	
+	public static singleton getInstance() {
+		return singletonInstance;
+	}
+	
+	private singleton() {
+		list = new ArrayList<String>();
+	}
+        
+        public ArrayList<String> getList() {
+		return this.list;
+	}
+	
+	public void addToList(String element) {
+		list.add(element);
+	}
+	
+	public void removeFromList(int elementIndex) {
+		list.remove(elementIndex);
+	}
+
+}
