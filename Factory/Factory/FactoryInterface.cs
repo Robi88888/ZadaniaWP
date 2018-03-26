13ade99a87281df6fd8109cbfa24bed35f49f5d5
Robi88888 <34077835+Robi88888@users.noreply.github.com>
2018-03-26 21:02:01 +0200

    Dodanie interfejsu

---
+++ Factory/Factory/FactoryInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    interface FactoryInterface
+    {
+        String addItem();
+        String deleteItem();
+        String listItems();
+        String sortItems();
+    }
+}
