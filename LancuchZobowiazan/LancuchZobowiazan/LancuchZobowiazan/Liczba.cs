7e5708c6dfd49a91134db86080b60f7f2cde2f0d
Robi88888 <34077835+Robi88888@users.noreply.github.com>
2018-05-13 11:56:34 +0200

    Klasa przechowująca obiekt

---
+++ LancuchZobowiazan/LancuchZobowiazan/LancuchZobowiazan/Liczba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LancuchZobowiazan
+{
+    abstract class Liczba
+    {
+        protected Liczba liczba;
+
+        public void UstawLiczbe(Liczba liczba)
+        {
+            this.liczba = liczba;
+        }
+
+        public abstract void Zadanie(WielkoscLiczby wielkoscliczby); // Zadanie -> Żądanie :P
+    }
+}
