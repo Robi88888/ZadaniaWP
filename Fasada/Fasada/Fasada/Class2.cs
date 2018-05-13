bf6257a9a69a78b02c7e5b20cb2d923e1c406986
Robi88888 <34077835+Robi88888@users.noreply.github.com>
2018-05-13 11:30:03 +0200

    Zapomniałem o Mainie

---
+++ Fasada/Fasada/Fasada/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasada
+{
+    class Class2
+    {
+        static void Main(string[] args)
+        {
+            var fasada = new FasadaPostaci(new KlasaPostaci(), new AtrybutyPostaci(), new UmiejetnosciPostaci(), new WygladPostaci());
+
+            fasada.UtworzPostac();
+
+            Console.ReadKey();
+        }
+    }
+}
