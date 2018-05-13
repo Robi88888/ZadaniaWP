b11c55dba3fbb6a9046677b4653baa1de7a530cf
Robi88888 <34077835+Robi88888@users.noreply.github.com>
2018-05-13 14:06:44 +0200

    Kompozyt

---
+++ Kompozyt/Kompozyt/Kompozyt/Poddany.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    interface Poddani
+    {
+        void InformacjeOPoddanym();
+    }
+
+
+class Poddany : Poddani
+{
+    private string Imie, Przydomek, Godnosc;
+
+    public Poddany(string imie, string przydomek, string godnosc)
+    {
+        Imie = imie;
+        Przydomek = przydomek;
+        Godnosc = godnosc;
+    }
+
+    public void InformacjeOPoddanym()
+    {
+        Console.WriteLine("Poddani: " + Imie + " " + Przydomek);
+    }
+}
+
+
+class Krol : Poddani
+{
+    private string Imie, Przydomek, Godnosc;
+    private List<Poddani> poddanii = new List<Poddani>();
+
+    public Krol(string imie, string przydomek, string godnosc)
+    {
+        Imie = imie;
+        Przydomek = przydomek;
+        Godnosc = godnosc;
+    }
+
+    public void DodajPoddanego(Poddani poddany)
+    {
+        poddanii.Add(poddany);
+    }
+
+    public void InformacjeOPoddanym()
+    {
+        Console.WriteLine("Poddani: " + Imie + " " + Przydomek);
+        foreach (Poddani poddany in poddanii)
+        {
+            poddany.InformacjeOPoddanym();
+        }
+    }
+}
---
+++ Kompozyt/Kompozyt/Kompozyt/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Krol krol = new Krol("Bolesław", "Chrobry", "Władca Polan");
+        krol.DodajPoddanego(new Poddany("Mścigniew", "Płocki", "Kasztelan Płocka"));
+        krol.DodajPoddanego(new Poddany("Dobromir", "Łęczycki", "Kasztelan Łęczycy"));
+        krol.DodajPoddanego(new Poddany("Miecław", "Kaliski", "Kasztelan Kalisza"));
+
+        krol.InformacjeOPoddanym();
+        Console.ReadKey();
+    }
+}
