b71f7ce2bcb29fb2ffdc8688091c43a57ae1c3a0
Robi88888 <34077835+Robi88888@users.noreply.github.com>
2018-05-13 11:15:33 +0200

    Klasy tworzące postać

---
+++ Fasada/Fasada/Fasada/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasada
+{
+   public interface WybKlasaPostaci
+   {
+        void UstawKlase();
+   }
+
+   public class KlasaPostaci : WybKlasaPostaci
+   {
+        public void UstawKlase()
+        {
+            Console.WriteLine("Klasa postaci - Wybierz Klase");
+        }
+   }
+
+    public interface WybAtrybutyPostaci
+    {
+        void UstawAtrybuty();
+    }
+
+    public class AtrybutyPostaci : WybAtrybutyPostaci
+    {
+        public void UstawAtrybuty()
+        {
+            Console.WriteLine("Atrybuty postaci - Wybierz Atrybuty");
+        }
+    }
+
+    public interface WybUmiejetnosciPostaci
+    {
+        void UstawUmiejetnosci();
+    }
+
+    public class UmiejetnosciPostaci : WybUmiejetnosciPostaci
+    {
+        public void UstawUmiejetnosci()
+        {
+            Console.WriteLine("Umiejętności postaci - Wybierz Umiejętności");
+        }
+    }
+
+    public interface WybWygladPostaci
+    {
+        void UstawWyglad();
+    }
+
+    public class WygladPostaci : WybWygladPostaci
+    {
+        public void UstawWyglad()
+        {
+            Console.WriteLine("Wygląd postaci - Wybierz Wygląd");
+        }
+    }
+}
