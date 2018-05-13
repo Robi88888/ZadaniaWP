9071e851f09930a34f18a4675552690f915a26a7
Robi88888 <34077835+Robi88888@users.noreply.github.com>
2018-05-13 12:06:45 +0200

    Kolejne klasy łańcucha

--- LancuchZobowiazan/LancuchZobowiazan/LancuchZobowiazan/Jeden.cs
+++ LancuchZobowiazan/LancuchZobowiazan/LancuchZobowiazan/Jeden.cs
@@ -20,4 +20,34 @@ namespace LancuchZobowiazan
             }
         }
     }
+
+    class Dwa : Liczba
+    {
+        public override void Zadanie(WielkoscLiczby wielkoscliczby)
+        {
+            if (wielkoscliczby == WielkoscLiczby.Dwa)
+            {
+                Console.WriteLine("Drugie żądanie");
+            }
+            else if (liczba != null)
+            {
+                liczba.Zadanie(wielkoscliczby);
+            }
+        }
+    }
+
+    class Trzy : Liczba
+    {
+        public override void Zadanie(WielkoscLiczby wielkoscliczby)
+        {
+            if (wielkoscliczby == WielkoscLiczby.Trzy)
+            {
+                Console.WriteLine("Trzecie żądanie");
+            }
+            else if (liczba != null)
+            {
+                liczba.Zadanie(wielkoscliczby);
+            }
+        }
+    }
 }
