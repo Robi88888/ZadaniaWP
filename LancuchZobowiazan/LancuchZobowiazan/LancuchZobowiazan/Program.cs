28ca55846ed4fdaa9366da3deef843fddd818ab0
Robi88888 <34077835+Robi88888@users.noreply.github.com>
2018-05-13 12:11:47 +0200

    Uzupełnienie programu

--- LancuchZobowiazan/LancuchZobowiazan/LancuchZobowiazan/Program.cs
+++ LancuchZobowiazan/LancuchZobowiazan/LancuchZobowiazan/Program.cs
@@ -16,6 +16,26 @@ namespace LancuchZobowiazan
     {
         static void Main(string[] args)
         {
+            Liczba jeden = new Jeden();
+            Liczba dwa = new Dwa();
+            Liczba trzy = new Trzy();
+
+            jeden.UstawLiczbe(dwa);
+            dwa.UstawLiczbe(trzy);
+
+            List<WielkoscLiczby> Zapytanie = new List<WielkoscLiczby>
+            {
+                WielkoscLiczby.Jeden,
+                WielkoscLiczby.Dwa,
+                WielkoscLiczby.Trzy
+            };
+
+            foreach (var quest in Zapytanie)
+            {
+                jeden.Zadanie(quest);
+            }
+
+            Console.ReadKey();
         }
     }
 }
