e78d4fb4831a8db6c6b872e1ba7e4c38c0682d4c
Robi88888 <34077835+Robi88888@users.noreply.github.com>
2018-05-13 12:04:58 +0200

    Dodanie pierwszej kalsy łąńcucha

---
+++ LancuchZobowiazan/LancuchZobowiazan/LancuchZobowiazan/Jeden.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LancuchZobowiazan
+{
+    class Jeden : Liczba
+    {
+        public override void Zadanie(WielkoscLiczby wielkoscliczby)
+        {
+            if(wielkoscliczby == WielkoscLiczby.Jeden)
+            {
+                Console.WriteLine("Pierwsze żądanie");
+            }
+            else if(liczba != null)
+            {
+                liczba.Zadanie(wielkoscliczby);
+            }
+        }
+    }
+}
--- LancuchZobowiazan/LancuchZobowiazan/LancuchZobowiazan/Program.cs
+++ LancuchZobowiazan/LancuchZobowiazan/LancuchZobowiazan/Program.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace LancuchZobowiazan
 {
-
+    public enum WielkoscLiczby
+    {
+        Jeden,
+        Dwa,
+        Trzy
+    }
     class program
     {
         static void Main(string[] args)
