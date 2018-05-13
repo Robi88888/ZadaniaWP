edfc9f6b159d8da4ab6605e37bc82a5fe121b308
Robi88888 <34077835+Robi88888@users.noreply.github.com>
2018-05-13 11:23:28 +0200

    Stworzenie fasady

---
+++ Fasada/Fasada/Fasada/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasada
+{
+    public class FasadaPostaci
+    {
+        private readonly WybKlasaPostaci klasa;
+        private readonly WybAtrybutyPostaci atrybuty;
+        private readonly WybUmiejetnosciPostaci umiejetnosci;
+        private readonly WybWygladPostaci wyglad;
+
+        public FasadaPostaci(WybKlasaPostaci klasa, WybAtrybutyPostaci atrybuty, WybUmiejetnosciPostaci umiejetnosci, WybWygladPostaci wyglad)
+        {
+            this.klasa = klasa;
+            this.atrybuty = atrybuty;
+            this.umiejetnosci = umiejetnosci;
+            this.wyglad = wyglad;
+        }
+
+        public void UtworzPostac()
+        {
+            Console.WriteLine("Utwórz postać");
+            klasa.UstawKlase();
+            atrybuty.UstawAtrybuty();
+            umiejetnosci.UstawUmiejetnosci();
+            wyglad.UstawWyglad();
+
+            Console.WriteLine("Postać zostałą stworzona");
+        }
+    }
+}
